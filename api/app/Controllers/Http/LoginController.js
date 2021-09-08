ab9ab0e5b2216f8d0b450d8024393fee8da28d0f
Pedro Schneider <pedro.schneider@cwi.com.br>
2021-09-07 21:40:07 -0300

    Adding route to logout and fixing invalidate token

--- api/app/Controllers/Http/LoginController.js
+++ api/app/Controllers/Http/LoginController.js
@@ -1,20 +1,39 @@
 'use strict'
 
+const User = use('App/Models/User')
+
 class LoginController {
   async create ({ request, auth }) {
     const { email, password } = request.all()
 
     const token = await auth.withRefreshToken().attempt(email, password)
 
+    token.routes = {
+      '/users': 'GET',
+      '/login/refresh?refreshToken=REFRESH_TOKEN': 'GET',
+      '/logout': 'GET',
+    }
+
     return token
   }
 
   async refresh ({ request, auth }) {
     const { refreshToken } = request.all()
+    const token = auth.getAuthHeader()
 
-    const token = await auth.newRefreshToken().generateForRefreshToken(refreshToken)
+    const newToken = await auth.newRefreshToken().generateForRefreshToken(refreshToken)
 
-    return token
+    await auth.revokeTokens([token, refreshToken], true)
+
+    return newToken
+  }
+
+  async logout ({ auth }) {
+    const user = await auth.getUser()
+
+    await auth.revokeTokensForUser(user, true)
+
+    return { message: 'Bye' }
   }
 }
 
